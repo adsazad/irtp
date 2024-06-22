a869c38b78d502ff3e9be4e29ae371fb01f63e0d
Arashdeep Singh <arashdeeps2004@gmail.com>
2024-06-22 22:36:49 +0530

    CLI TOOLS

---
+++ src/cli-tools/cli-tools.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    
+    
+    std::cout << "ITPR CLI Tools" << std::endl;
+    std::cout << "command available" << std::endl;
+    std::cout << "Create Room: cr" << std::endl;
+
+    std::string command;
+    std::cout << "Enter command: ";
+    std::cin >> command;
+    return 0;
+}
